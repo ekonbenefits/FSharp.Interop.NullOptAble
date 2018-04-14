0981b488dfa08a4e2a7b7d95964165f9509982d3
jbtule <jay+code@tuley.name>
2018-04-13 21:47:23 -0500

    Added Some unit tests :exclamation:

---
+++ tests/FSharp.Core.Experimental.OptionBuilder.Tests/Tests.fs
@@ -0,0 +1,54 @@
+module Tests
+
+open System
+open Xunit
+open FsUnit.Xunit
+open FSharp.Core.Experimental.OptionBuilder
+
+[<Fact>]
+let ``Basic Option Unwrap`` () =
+    option {
+        let! x = Some(3)
+        return x + 3
+    } |> should equal (Some 6)
+
+[<Fact>]
+let ``Basic Option None`` () =
+    option {
+        let! x = None
+        return x + 3
+    } |> should equal None
+
+[<Fact>]
+let ``Basic Nullable`` () =
+    option {
+        let! x = Nullable 3
+        return x + 4
+    } |> should equal (Some 7)
+
+[<Fact>]
+let ``Basic None `` () =
+    option {
+        let! x = Nullable()
+        return x + 3
+    } |> should equal None
+
+[<Fact>]
+let ``Basic Null allowed`` () =
+    option {
+        let! x = "Hello"
+        return x + " World"
+    } |> should equal (Some("Hello World"))
+
+[<Fact>]
+let ``Basic Null`` () =
+    option {
+        let! x = null
+        return x + " World"
+    } |> should equal None
+
+[<Fact>]
+let ``Basic sequence test`` () =
+    seqOption {
+        yield! Some 3
+    } |> should equal (seq { yield 3 })
