c57dbe8702ce7a53bd389838f3df78cfb76f8d8f
jbtule <jay+code@tuley.name>
2018-04-26 11:57:16 -0500

    spacing comments modified

--- src/FSharp.Interop.NullOptAble/Operators.fs
+++ src/FSharp.Interop.NullOptAble/Operators.fs
@@ -3,14 +3,15 @@ namespace FSharp.Interop.NullOptAble
 module Operators =
     open System
 
-    //static generic inlining inspired by http://stackoverflow.com/a/2812306/637783
+   
     type NullCoalesce =  
         static member Coalesce(a: 'a option, b: 'a Lazy) = a |> Option.defaultWith b.Force
         static member Coalesce(a: 'a Nullable, b: 'a Lazy) = a |> Option.ofNullable 
                                                                |> Option.defaultWith b.Force
         static member Coalesce(a: 'a when 'a:null, b: 'a Lazy) = a |> Option.ofObj 
                                                                    |> Option.defaultWith b.Force
-
+    
+    //static generic overloading inlining inspired by http://stackoverflow.com/a/2812306/637783
     let inline nullCoalesceHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Coalesce : ^a * ^b -> ^c)> a b = 
                                                 ((^t or ^a) : (static member Coalesce : ^a * ^b -> ^c) (a, b))
 
@@ -19,14 +20,12 @@ module Operators =
 
 
     type NullMap =  
-
         static member Into(a: 'a option, f: 'a -> 't) = Option.map f a
-
         static member Into(a: 'a Nullable, f: 'a -> 't) = a |> Option.ofNullable
                                                             |> Option.map f 
         static member Into(a: 'a when 'a:null, f: 'a -> 't) = a |> Option.ofObj
                                                                 |> Option.map f
-
+    //static generic overloading inlining inspired by http://stackoverflow.com/a/2812306/637783
     let inline nullIntoHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Into : ^a * ^b -> ^c)> a b = 
                                                 ((^t or ^a) : (static member Into : ^a * ^b -> ^c) (a, b))
 
