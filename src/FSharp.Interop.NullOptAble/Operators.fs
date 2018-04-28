717bd499f937bd576d68ad3fc600f1c8759a9dcb
jbtule <jay+code@tuley.name>
2018-04-27 23:10:26 -0500

    cleaned up formatting

--- src/FSharp.Interop.NullOptAble/Operators.fs
+++ src/FSharp.Interop.NullOptAble/Operators.fs
@@ -5,32 +5,37 @@ module Operators =
     
     //static overloading operators inspired by http://stackoverflow.com/a/2812306/637783
     let inline defaultWithOverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member DefaultWith : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member DefaultWith : ^a * ^b -> ^c) (a, b))
+        ((^t or ^a) : (static member DefaultWith : ^a * ^b -> ^c) (a, b))
     let inline mapOverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Map : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Map : ^a * ^b -> ^c) (a, b))
+        ((^t or ^a) : (static member Map : ^a * ^b -> ^c) (a, b))
     let inline map2OverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Map2 : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Map2 : ^a * ^b -> ^c) (a, b))
+        ((^t or ^a) : (static member Map2 : ^a * ^b -> ^c) (a, b))
     let inline map3OverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Map3 : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Map3 : ^a * ^b -> ^c) (a, b))
+        ((^t or ^a) : (static member Map3 : ^a * ^b -> ^c) (a, b))
     let inline bindOverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Bind : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Bind : ^a * ^b -> ^c) (a, b))                                          
-
+        ((^t or ^a) : (static member Bind : ^a * ^b -> ^c) (a, b))
     let inline bind2OverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Bind2 : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Bind2 : ^a * ^b -> ^c) (a, b))                                          
+        ((^t or ^a) : (static member Bind2 : ^a * ^b -> ^c) (a, b))
     let inline bind3OverloadHelper< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Bind3 : ^a * ^b -> ^c)> a b = 
-                                                ((^t or ^a) : (static member Bind3 : ^a * ^b -> ^c) (a, b))                                          
+        ((^t or ^a) : (static member Bind3 : ^a * ^b -> ^c) (a, b))
 
-
+    (* default with *)
     let inline ( |?-> ) a b = defaultWithOverloadHelper<NullOptAble, _, _, _> a b
+    (* map *)
     let inline ( |>?@ ) a b = mapOverloadHelper<NullOptAble, _, _, _> a b
     let inline ( @?<| ) b a = mapOverloadHelper<NullOptAble, _, _, _> a b
+    (* map2 *)
     let inline ( ||>?@ ) a b = map2OverloadHelper<NullOptAble, _, _, _> a b
     let inline ( @?<|| ) b a = map2OverloadHelper<NullOptAble, _, _, _> a b
+    (* map3 *)
     let inline ( |||>?@ ) a b = map3OverloadHelper<NullOptAble, _, _, _> a b
     let inline ( @?<||| ) b a = map3OverloadHelper<NullOptAble, _, _, _> a b
+    (* bind *)
     let inline ( |>? ) a b = bindOverloadHelper<NullOptAble, _, _, _> a b
     let inline ( ?<| ) b a = bindOverloadHelper<NullOptAble, _, _, _> a b
+    (* bind2 *)
     let inline ( ||>? ) a b = bind2OverloadHelper<NullOptAble, _, _, _> a b
     let inline ( ?<|| ) b a = bind2OverloadHelper<NullOptAble, _, _, _> a b
+    (* bind3 *)
     let inline ( |||>? ) a b = bind3OverloadHelper<NullOptAble, _, _, _> a b
     let inline ( ?<||| ) b a = bind3OverloadHelper<NullOptAble, _, _, _> a b
--- src/FSharp.Interop.NullOptAble/Operators.fsi
+++ src/FSharp.Interop.NullOptAble/Operators.fsi
@@ -1,27 +1,34 @@
 namespace FSharp.Interop.NullOptAble
   module Operators = begin
+    (* default with *)
     val inline ( |?-> ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member DefaultWith : ^a * ^b -> ^c)
+    (* map *)
     val inline ( |>?@ ) : a: ^a -> b: ^b ->  ^c
         when (NullOptAble or  ^a) : (static member Map : ^a * ^b -> ^c)
     val inline ( @?<| ) : b: ^a -> a: ^b -> ^c
         when (NullOptAble or  ^b) : (static member Map : ^b * ^a -> ^c)
+    (* map2 *)
     val inline ( ||>?@ ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member Map2 : ^a * ^b -> ^c)
     val inline ( @?<|| ) : b: ^a -> a: ^b -> ^c
         when (NullOptAble or  ^b) : (static member Map2 : ^b * ^a -> ^c)
+    (* map3 *)
     val inline ( |||>?@ ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member Map3 : ^a * ^b -> ^c)
     val inline ( @?<||| ) : b: ^a -> a: ^b -> ^c
         when (NullOptAble or  ^b) : (static member Map3 : ^b * ^a -> ^c)
+    (* bind *)
     val inline ( |>? ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member Bind : ^a * ^b -> ^c)
     val inline ( ?<| ) : b: ^a -> a: ^b -> ^c
         when (NullOptAble or  ^b) : (static member Bind : ^b * ^a -> ^c)
+    (* bind2 *)
     val inline ( ||>? ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member Bind2 : ^a * ^b -> ^c)
     val inline ( ?<|| ) : b: ^a -> a: ^b -> ^c
         when (NullOptAble or  ^b) : (static member Bind2 : ^b * ^a -> ^c)
+    (* bind3 *)
     val inline ( |||>? ) : a: ^a -> b: ^b -> ^c
         when (NullOptAble or  ^a) : (static member Bind3 : ^a * ^b -> ^c)
     val inline ( ?<||| ) : b: ^a -> a: ^b -> ^c
