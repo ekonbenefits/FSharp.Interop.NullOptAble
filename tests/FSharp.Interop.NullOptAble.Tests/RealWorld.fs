765d6968b1ae07721dbcac8ac3941ce51991c1ad
jbtule <jay+code@tuley.name>
2018-04-28 12:30:55 -0500

    Trying out some literate formatting

--- tests/FSharp.Interop.NullOptAble.Tests/RealWorld.fs
+++ tests/FSharp.Interop.NullOptAble.Tests/RealWorld.fs
@@ -1,11 +1,21 @@
-module RealWorldTests
+(**
+Real World-ish examples
+===============
+**)
 
+module RealWorldTests
 open System
+open System.Text
+(**hide**)
 open Xunit
+(**hide**)
 open FsUnit.Xunit
 open FSharp.Interop.NullOptAble
-open System.Text
 
+
+(**
+Just your basic bind of two nullable variables.
+**)
 [<Fact>]
 let ``Basic nullable math`` () =
     let x = Nullable(3)
@@ -16,14 +26,19 @@ let ``Basic nullable math`` () =
         return (x' + y')
     } |> should equal (Some 6)
 
+(**Hide**)
 [<AllowNullLiteral>]
 type Node (child:Node)=
     new() = new Node(null)
     member val child:Node = child with get,set
-    
+ 
+(**
+    Doing the things found in this [MSDN Blog Post][1] did with the Safe Nav operator.
+    [1]:https://blogs.msdn.microsoft.com/jerrynixon/2014/02/26/at-last-c-is-getting-sometimes-called-the-safe-navigation-operator/
+**)
 [<Fact>]
 let ``Safe Navigation Operator Example`` ()=
-    // https://blogs.msdn.microsoft.com/jerrynixon/2014/02/26/at-last-c-is-getting-sometimes-called-the-safe-navigation-operator/
+    
     let parent = Node()
     option {
         let! a = parent.child
