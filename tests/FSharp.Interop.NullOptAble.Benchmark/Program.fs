e16da20c6fa2a820d84fd2c612648881c84c6325
jbtule <jay+code@tuley.name>
2018-05-08 10:26:24 -0500

    Added some inline comparisons

--- tests/FSharp.Interop.NullOptAble.Benchmark/Program.fs
+++ tests/FSharp.Interop.NullOptAble.Benchmark/Program.fs
@@ -10,7 +10,7 @@ open BenchmarkDotNet.Running
 open FSharp.Interop.NullOptAble
 open FSharp.Interop.NullOptAble.Experimental.ValueOption
 
-[<RPlotExporter; RankColumn;MemoryDiagnoser>]
+[<RankColumn;MemoryDiagnoser>]
 type BenchmarkValueOption () =
 
     let r = Random();
@@ -24,8 +24,13 @@ type BenchmarkValueOption () =
     member this.Setup() =
         data <- Array.init this.N (fun _-> nucleo.[r.Next(0, nucleo.Length)])
                 |> System.String
+        if (this.RefOption() <> this.ValueOption()
+                || this.RefOption() <> this.RefOptionInline()
+                || this.RefOption() <> this.ValueOptionInline()) then
+            failwith "implementations don't match!"
 
-    [<Benchmark(Baseline = true)>]
+    
+    [<Benchmark>]
     member __.RefOption()=
         let toRna (dna: string): string option = 
             let combine (sb:StringBuilder option) =
@@ -65,6 +70,50 @@ type BenchmarkValueOption () =
                 let! sb' = dna' |> Seq.fold combine (VSome <| StringBuilder())
                 return sb'.ToString()
             }
+        data |> toRna |> ValueOption.defaultValue ""
+
+    [<Benchmark(Baseline = true)>]
+    member __.RefOptionInline()=
+        let toRna (dna: string): string option = 
+            let combine (sb:StringBuilder option) =
+                let append (c:char) =
+                    match sb with
+                    | Some sb' -> sb'.Append(c) |> ignore
+                                  sb
+                    | None -> sb
+                function
+                | 'G' -> 'C' |> append
+                | 'C' -> 'G' |> append
+                | 'T' -> 'A' |> append
+                | 'A' -> 'U' |> append
+                | ___ -> None
+            match dna with
+            | null -> None
+            | dna' -> match dna' |> Seq.fold combine (Some <| StringBuilder()) with
+                      | Some sb' -> sb'.ToString() |> Some
+                      | None -> None
+        data |> toRna |> Option.defaultValue ""
+
+    [<Benchmark>]
+    member __.ValueOptionInline()=
+        let toRna (dna: string): string voption = 
+            let combine (sb:StringBuilder voption) =
+                let append (c:char) =
+                    match sb with
+                    | VSome sb' -> sb'.Append(c) |> ignore
+                                   sb
+                    | VNone -> sb
+                function
+                | 'G' -> 'C' |> append
+                | 'C' -> 'G' |> append
+                | 'T' -> 'A' |> append
+                | 'A' -> 'U' |> append
+                | ___ -> VNone
+            match dna with
+            | null -> VNone
+            | dna' -> match dna' |> Seq.fold combine (VSome <| StringBuilder()) with
+                      | VSome sb' -> sb'.ToString() |> VSome
+                      | VNone -> VNone
         data |> toRna |> ValueOption.defaultValue ""
 
 [<EntryPoint>]
